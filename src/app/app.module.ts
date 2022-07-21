b0372253f16fbc80408c7d85e14057ba0ee33958
Clinton wambugu <clintonwambugu@gmail.com>
2022-07-21 12:21:41 +0300

    Add customers list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CustomersModule } from './customers/customers.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CustomersModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
